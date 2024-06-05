bde1faccb5546d3f1d401ce2a2d5e243532ffb63
ikkeGyldig <47673804+ikkeGyldig@users.noreply.github.com>
2024-06-05 07:32:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"MESSAGE": "It's working - mark test commit! => Another test commit"}
+    return {"MeSsAge": "It's working - mark test commit! => Another test commit"}
