59076b5399811e8b1fc7a37a850a41e3cd43cb50
ikkeGyldig <47673804+ikkeGyldig@users.noreply.github.com>
2024-06-05 08:10:52 -0400

    nothing literally added

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"MeSsAge": "It's working - mark test commit! => Another test commit"}
+    return {"MeSsAge": "Nothing's working - mark test commit! => Another test commit"}
